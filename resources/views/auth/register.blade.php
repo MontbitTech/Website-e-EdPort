e54880e38942cb4a109cf2f66737507df9632c32
anshuman <anshumannrt@gmail.com>
2020-10-12 13:35:22 +0530

    changes in register blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -60,14 +60,37 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
-                        <div class="form-group row">
+                       <!--  <div class="form-group row">
                             <div class="col-md-6 offset-md-4 mr-0 pr-0">
                                 <input type="radio" class="ml-1" name="role" value="admin" required checked>
-                                <label for="role" class="ml-2 text-md-left">{{ __('I am Admin') }}</label>
+                                <label for="role" class="ml-2 text-md-left">{{ __('Product') }}</label>
                                 <input type="radio" class="ml-1" name="role" value="user">
-                                <label for="role" class="ml-2 text-md-left">{{ __('I am User') }}</label>
+                                <label for="role" class="ml-2 text-md-left">{{ __('Service') }}</label>
                             </div>
 
+                        </div> -->
+
+                        <div class="form-group row">
+                            <label for="demo" class="col-md-4 col-form-label text-md-right">{{ __('Demo Request') }}</label>
+
+                            <div class="col-md-6">
+
+                                <select class="form-control" name="role" id="role" required>
+                                <option value="" selected>Select</option>
+                                <option value="e-EdPort Virtual classroom solution">e-EdPort Virtual classroom solution</option>
+                                <option value="e-EdPort Student Self Learning Solution">e-EdPort Student Self Learning Solution</option>
+                                <option value="e-EdPort Parental Control App">e-EdPort Parental Control App</option>
+                                <option value="e-EdPort Smart Teacher Training">e-EdPort Smart Teacher Training</option>
+                                <option value="e-EdPort Smart Teacher Certification">e-EdPort Smart Teacher Certification</option>
+                                <option value="e-EdPort Student Career Counseling">e-EdPort Student Career Counseling</option>
+                                </select>
+
+                                @error('demo')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
                         </div>
 
                         <div class="form-group row mb-0">
