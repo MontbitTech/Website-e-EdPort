de4bace19d63862c8fdd1eea775586e24076287d
vikram700 <vs4110690@gmail.com>
2020-10-13 11:56:04 +0530

    select2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    .selection {
+        display: block !important;
+    }
+</style>
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -60,7 +67,7 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
-                       <!--  <div class="form-group row">
+                        <!--  <div class="form-group row">
                             <div class="col-md-6 offset-md-4 mr-0 pr-0">
                                 <input type="radio" class="ml-1" name="role" value="admin" required checked>
                                 <label for="role" class="ml-2 text-md-left">{{ __('Product') }}</label>
@@ -72,16 +79,13 @@
 
                         <div class="form-group row">
                             <label for="demo" class="col-md-4 col-form-label text-md-right">{{ __('Request Type') }}</label>
-
-                            <div class="col-md-6">
-
-                                <select class="form-control" name="role[]" id="role" required multiple>
-                                <!-- <option value="" selected>Select</option> -->
-                                <option value="e-EdPort Virtual Classroom Solution">e-EdPort Virtual Classroom Solution</option>
-                                <option value="e-EdPort Student Self Learning Solution">e-EdPort Student Self Learning Solution</option>
-                                 <option value="e-EdPort Parental Control App">e-EdPort Parental Control App</option>
-                                 <option value="e-EdPort Smart Teacher Training">e-EdPort Smart Teacher Training</option>
-                                 <option value="e-EdPort Smart Teacher Certification">e-EdPort Smart Teacher Certification</option>
+                            <div class="col-md-6 col-12">
+                                <select class="form-control w-100" name="role[]" id="role" required multiple>
+                                    <option value="e-EdPort Virtual Classroom Solution">e-EdPort Virtual Classroom Solution</option>
+                                    <option value="e-EdPort Student Self Learning Solution">e-EdPort Student Self Learning Solution</option>
+                                    <option value="e-EdPort Parental Control App">e-EdPort Parental Control App</option>
+                                    <option value="e-EdPort Smart Teacher Training">e-EdPort Smart Teacher Training</option>
+                                    <option value="e-EdPort Smart Teacher Certification">e-EdPort Smart Teacher Certification</option>
                                 </select>
 
                                 @error('demo')
@@ -107,8 +111,6 @@
 </div>
 
 <script>
-    $(function() {
-        $('#role').select2();
-    })
+    $('#role').select2();
 </script>
 @endsection
