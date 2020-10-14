3c290ae3825310c6f63ebf8bafc99d1cbcfdaf63
vikram700 <vs4110690@gmail.com>
2020-10-14 21:37:12 +0530

    hgjh

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -22,9 +22,9 @@ $(function() {
     
      //===== close navbar-collapse when a  clicked
     
-    $(".navbar-nav a").on('click', function () {
-        $(".navbar-collapse").removeClass("show");
-    });
+    // $(".navbar-nav a").on('click', function () {
+    //     $(".navbar-collapse").removeClass("show");
+    // });
     
     //===== Mobile Menu
     
@@ -32,9 +32,9 @@ $(function() {
         $(this).toggleClass("active");
     });
     
-    $(".navbar-nav a").on('click', function() {
-        $(".navbar-toggler").removeClass('active');
-    });
+    // $(".navbar-nav a").on('click', function() {
+    //     $(".navbar-toggler").removeClass('active');
+    // });
     
     
     //===== Section Menu Active
